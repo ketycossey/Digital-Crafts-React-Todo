773b57ed8a4718f4da9f54d4ae5ff16f70743e05
fuskovic <fhuskovic92@gmail.com>
2019-02-20 16:37:39 -0500

    finish exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      val : '',
+      tasks: []
+    }
+  }
+
+handleChange(input){
+    this.setState({ val : input})
+}
+
+
+handleSubmit(e){
+    e.preventDefault()
+    let updatedToDoItems = [...this.state.tasks]
+    updatedToDoItems.push({task : this.state.val, isCompleted: false})
+    this.setState({ val: "", tasks : updatedToDoItems})
+}
+
+toggleCompletion(task){
+  let indx = this.state.tasks.indexOf(task)
+  let newTaskState = [...this.state.tasks]
+  newTaskState[indx].isCompleted = !newTaskState[indx].isCompleted 
+  this.setState({items: newTaskState})
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+         <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input type="text" value={this.state.val} onChange={(e) => this.handleChange(e.target.value)}></input>
+          <button type="submit">Add</button>
+        </form>
+        <h3>To Do List</h3>
+        <ul>{
+          this.state.tasks.filter(item => item.isCompleted === false).map((item,i) => <li key={i}>{item.task}<button onClick={() => this.toggleCompletion(item)}>Mark as Done</button></li>)
+        }</ul>
+        <h3>Completed</h3>
+        <ul>{
+          this.state.tasks.filter(item => item.isCompleted === true).map((item,i) => <li key={i}>{item.task}<button onClick={() => this.toggleCompletion(item)}>Restore</button></li>)
+        }</ul>
       </div>
     );
   }
